88a7a4eec9ba3ce472fc8fc8e2f0da2509e33a25
Dominique Miller <dominique.p.miller@gmail.com>
2015-12-15 07:47:57 -0500

    added form validations

--- trim/app/models/user.rb
+++ trim/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  validates :fullname, :email, :repo, :url, presence: true, length: { maximum: 200 }
+  validates :email, uniqueness: true
+  validates :oop, :modular, :workflow, :testing, :database, :problem, :javascript,
+            :html, :css, :team, :self, :communication, :debugging, presence: true,
+            numericality: { greater_than_or_equal_to: 0 }
 end
