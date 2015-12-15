4ad903ff3c113b4f1915b1c764c13f0fcbe2aafb
Dominique Miller <dominique.p.miller@gmail.com>
2015-12-15 08:54:47 -0500

    fixed font issue added root and friendy url routes

--- trim/config/routes.rb
+++ trim/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :users
+
+  get "/welcome" => "users#index"
+  get "/apprentice" => "users#new"
+
+  root to: "users#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
